775c17914c40c81ee12d014966fd31d7b2de797a
Shantanu Wadode <110329245+ShantanuWadode@users.noreply.github.com>
2024-02-18 17:00:21 +0530

    Update Meditation.js

--- frontend0/src/Components/Meditation.js
+++ frontend0/src/Components/Meditation.js
@@ -10,70 +10,19 @@ const Meditation = () => {
                 <div className="lg:max-w-lg">
                   
                   <p className="mt-2 text-3xl text-center font-bold tracking-tight text-gray-900 sm:text-4xl">Meditation</p>
+                  <p className="mt-1 text-1xl text-center  tracking-tight text-gray-400 sm:text-1xl">Sit in a comfortable position, focus on your breathing.</p>
                   <div className="mt-6 text-lg leading-8 text-gray-600">
-                  <form className="space-y-6" >
-                  <div>
-                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="name"
-                      name="name"
-                      type="name"
-                      autoComplete="name"
-                      required
-                     
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                      Password
-                    </label>
-                   
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </form>
+                  <div className="flex flex-col items-center justify-center space-y-4">
+      <button className="w-full bg-transparent border border-gray-300 hover:bg-gray-100 text-gray-900 font-medium py-2 px-4 rounded-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+        Silence
+      </button>
+      <button className="w-full bg-transparent border border-gray-300 hover:bg-gray-100 text-gray-900 font-medium py-2 px-4 rounded-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+        Only Music
+      </button>
+      <button className="w-full bg-transparent border border-gray-300 hover:bg-gray-100 text-gray-900 font-medium py-2 px-4 rounded-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+        Guided Meditation
+      </button>
+    </div>
                   </div>
                  
                 </div>
