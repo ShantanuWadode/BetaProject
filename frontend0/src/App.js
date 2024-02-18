c3ab32c70070bcc18ae440c20b0784968dcf8b79
Shantanu Wadode <110329245+ShantanuWadode@users.noreply.github.com>
2024-02-18 16:47:54 +0530

    Add files via upload

---
+++ frontend0/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Homepage from './Homepage';
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Mainpage from "./Components/Mainpage";
+// import Card from "./Components/Card";
+import Meditation from './Components/Meditation';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mainpage" element={isLoggedIn ? <Mainpage /> : <Navigate to="/login" />} />
+        <Route path="/meditation" element={<Meditation />} />
+        
+        {/* <Route path="/card" element={isLoggedIn ? <Card /> : <Navigate to="/login" />} /> */}
+        {/* Other routes */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend0/src/Components/Meditation.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import medimg from '../assets/Meditation.jpg'
+
+const Meditation = () => {
+    return (
+        <div className="overflow-hidden bg-white py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="lg:pr-8 lg:pt-4">
+                <div className="lg:max-w-lg">
+                  
+                  <p className="mt-2 text-3xl text-center font-bold tracking-tight text-gray-900 sm:text-4xl">Meditation</p>
+                  <div className="mt-6 text-lg leading-8 text-gray-600">
+                  <form className="space-y-6" >
+                  <div>
+                  <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="name"
+                      name="name"
+                      type="name"
+                      autoComplete="name"
+                      required
+                     
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Password
+                    </label>
+                   
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+                  </div>
+                 
+                </div>
+              </div>
+              <img
+                src={medimg}
+                alt="Meditation"
+                className="w-[37rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[37rem] md:-ml-4 "
+                width={2432}
+                height={1442}
+              />
+            </div>
+          </div>
+        </div>
+      )
+}
+
+export default Meditation
