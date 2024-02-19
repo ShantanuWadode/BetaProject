5687525978d5e3f062071b1f05eda3a3280b8a69
Vaibhav Tomar <vaibhavtomar0812@gmail.com>
2024-02-19 12:55:45 +0530

    added routes for VideoCall

--- frontend0/src/App.js
+++ frontend0/src/App.js
@@ -6,6 +6,8 @@ import Register from "./Components/Register";
 import Mainpage from "./Components/Mainpage";
 // import Card from "./Components/Card";
 import Meditation from './Components/Meditation';
+import VideoCall from './Components/VideoCall';
+import RoomPage from './Components/RoomPage';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,7 +19,9 @@ function App() {
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/mainpage" element={isLoggedIn ? <Mainpage /> : <Navigate to="/login" />} />
-        <Route path="/meditation" element={<Meditation />} />
+        <Route path="/meditation" element={<Meditation /> } />
+        <Route path="/videocall" element={<VideoCall /> } />
+        <Route path="/videocall/:id" element={<RoomPage /> } />
         
         {/* <Route path="/card" element={isLoggedIn ? <Card /> : <Navigate to="/login" />} /> */}
         {/* Other routes */}
